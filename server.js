619978fb5c6c55df840dba80affddddd681f205a
chris-laplante <40474653+chris-laplante@users.noreply.github.com>
2020-03-10 13:45:19 -0400

    Update server.js
    add error handlers

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 io.on('connect', onConnect);
+io.on('error', (error) => console.error('io error: ' + error));
 server.listen(port, () => console.log('server listening on port ' + port));
+server.on('error', (error) => console.error('server error: ' + error));
 
 function onConnect(socket){
   console.log('connect ' + socket.id);
 
+  socket.on('error', (error) => console.error('socket error: ' + error));
   socket.on('disconnect', () => console.log('disconnect ' + socket.id));
 }
